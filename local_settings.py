1822a178443db4b867fa57f8a2bf174e344cd83e
LoganPederson <76628309+LoganPederson@users.noreply.github.com>
2022-08-03 22:30:16 -0500

    Update local_settings.py
    Woopsie Daisey

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,1 @@
-postgresql = {
-    'pguser':'postgres',
-    'pgpasswd':'Scarlett22!',
-    'pghost':'simcompaniescalculator2.c4w8wmvikrna.us-east-2.rds.amazonaws.com',
-    'pgport':5432,
-    'pgdb':'simcompaniescalculator'
-}
+
